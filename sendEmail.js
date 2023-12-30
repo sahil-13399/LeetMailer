bd3c14bc6115d02c930779f21a964600b5d4b7ac
Sahil Samantaray <sahil13399@gmail.com>
2023-12-30 17:59:41 +0530

    Fixed auth connect issue in sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,28 +1,34 @@
 const nodemailer = require("nodemailer");
-const fs = require('fs/promises')
-const readHtmlTemplate = require("./emailTemplate")
+const fs = require("fs/promises");
+const readHtmlTemplate = require("./emailTemplate");
+let transporter = null;
 
 async function createTransporter() {
-    try {
-      const secretsData = await fs.readFile('./resources/secrets.json', 'utf-8');
-      const secrets = JSON.parse(secretsData);
-      
-      return nodemailer.createTransport({
-        service: 'gmail', // e.g., 'gmail'
-        auth: {
-          user: secrets.email.user,
-          pass: secrets.email.pass,
-        },
-      });
-    } catch (error) {
-      console.error('Error reading secrets file:', error);
-      throw error;
-    }
+  try {
+    const secretsData = await fs.readFile("./resources/secrets.json", "utf-8");
+    const secrets = JSON.parse(secretsData);
+
+    return nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: secrets.email.user,
+        pass: secrets.email.pass,
+      },
+    });
+  } catch (error) {
+    console.error("Error reading secrets file:", error);
+    throw error;
   }
+}
 
 async function sendEmail(email) {
   // send mail with defined transport object
-  const transporter = await createTransporter();
+  if (transporter == null) {
+    transporter = await createTransporter();
+  }
   const htmlTemplate = await readHtmlTemplate([
     { link: "https://leetcode.com/problems/first-leetcode-question" },
     { link: "https://leetcode.com/problems/second-leetcode-question" },
@@ -37,5 +43,5 @@ async function sendEmail(email) {
 
   console.log("Message sent: %s", info.messageId);
 }
-
+sendEmail("sahil13399@gmail.com")
 module.exports = sendEmail;
